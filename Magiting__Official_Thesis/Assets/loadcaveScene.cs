d23f5d0822d841e5663f9a3ffad4b9390a8ef3e7
Normzz1437 <normanzreal@gmail.com>
2024-01-09 23:41:07 +0800

    swamp Scene Finsished

--- Magiting__Official_Thesis/Assets/loadcaveScene.cs
+++ Magiting__Official_Thesis/Assets/loadcaveScene.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class loadcaveScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene("CaveScene_Final", LoadSceneMode.Single);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
